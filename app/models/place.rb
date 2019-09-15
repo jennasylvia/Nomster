b1e5eac28348c1dd804eb5717ae6e7e7482ad961
Jenna Sylvia <jenna.sylvia@berkeley.edu>
2019-09-15 23:20:12 +0000

    geocoder

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,7 @@
 class Place < ApplicationRecord
   belongs_to :user
-  validates :name, presence: true
+  geocoded_by :address
+  after_validation :geocode
+  
+  validates :description, presence: true
 end
