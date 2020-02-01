97141e1e9b3880a4b0b16b9ebeef796e908ca497
Jenna Sylvia <jenna.sylvia@berkeley.edu>
2020-02-01 03:11:37 +0000

    please god work

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,8 @@ module Nomster
 
     # Precompile additional assets
     config.assets.precompile += %w( .svg .eot .woff .ttf )
+
+    config.assets.initialize_on_precompile = false
     
   end
 end
